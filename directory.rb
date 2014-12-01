1aca9e9acb7733dd961c40b225bf8daae21c9348
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-01 19:57:06 +0000

    refactored code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 7
+# first we print the list of students
 puts "Some students in my cohort at Makers Academy"
 puts "---------------"
 puts "Andy Newman"
@@ -7,3 +9,5 @@ puts "Marcin Wal"
 puts "Emily Sas"
 puts "Marcus de Wilde"
 puts "Matteo Manzo"
+#finally we print the total
+print "Overall we have: #{student_count} great students."
