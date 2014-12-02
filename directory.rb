26a793f308e9e25e556c313cd7cba1d2c21e26dc
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-02 12:15:01 +0000

    Get the list of students from user

--- directory.rb
+++ directory.rb
@@ -1,14 +1,4 @@
-# first we print the list of students
 
-
-students = [{:name=>"Andy Newman", :cohort=>:december}, 
-            {:name=>"Keiran Goodacre", :cohort=>:december}, 
-            {:name=>"Bebbs", :cohort=>:december}, 
-            {:name=>"Marcin Wal", :cohort=>:december}, 
-            {:name=>"Emily Sas", :cohort=>:december}, 
-            {:name=>"Marcus de Wilde", :cohort=>:december}, 
-            {:name=>"Matteo Manzo", :cohort=>:december}
-]
 
 def print_header
   puts "Some students in my cohort at Makers Academy"
@@ -25,7 +15,28 @@ def print_footer(names)
   puts "Overall we have: #{names.length} great students."
 end
 
+
+def input_students
+  puts "Please enter the names of the students."
+  puts "To finish, just hit return twice"
+  # create an empty array 
+  students = []
+  # get the first name
+  name = gets.chomp
+  while !name.empty? do 
+    #add the student to the hash 
+    students << {:name => name, :cohort => :decemeber}
+    puts "Now we have #{students.length} students."
+    # get another name from the user 
+    name = gets.chomp
+  end 
+  # return the array of students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
 
