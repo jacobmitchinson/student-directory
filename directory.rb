3c8dd8075b167f1c475357cfba71bff30af9eaf3
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-03 15:13:23 +0000

    Refactored interactive menu into multiple methods

--- directory.rb
+++ directory.rb
@@ -1,61 +1,74 @@
+@students = []
 
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
-def display
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer    
-  puts "Overall, we have #{students.length} great students"
+  puts "Overall, we have #{@students.length} great students"
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
+  @students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do    
     # add the student hash to the array
-    students << {:name => name, :cohort => :november}    
-    puts "Now we have #{students.length} students"
+    @students << {:name => name, :cohort => :november}    
+    puts "Now we have #{@students.length} students"
     # get another name from the user
     name = gets.chomp
   end
 end
 
 def interative_menu
-  students = []
+  @students = []
   loop do 
-    # 1. print the menu and ask the user what to do 
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-      when "1"
-        # input the students
-        students = input_students
-      when "2"
-        # show the students
-        print_header
-        display(students)
-        print_footer(students)
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
+
+def print_menu
+  # 1. print the menu and ask the user what to do 
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+  # 2. read the input and save it into a variable
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+    # input the students
+      students = input_students
+    when "2"
+      # show the students
+    show_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again."
+  end
+end
+
+
 
 interative_menu
 
