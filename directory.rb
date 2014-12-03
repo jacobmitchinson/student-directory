cafc6e322ce429f0e022af8c4e4b4e365a794eaa
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-03 12:28:38 +0000

    changed master to remove exercises and created exercises branch

--- directory.rb
+++ directory.rb
@@ -1,140 +1,42 @@
-# need to refactor methods as they are becoming bloated 
 
-def header
-  "Some students in my cohort at Makers Academy".center(60) + "\n#{"-" * 60}\n"
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-def display(students)
-  count = students.length 
-
-  until count == 0
-    student = students[count - 1]
-
-    if student[:name].downcase.start_with?("a") && (student[:name].delete(' ').length. < 12)
-      print "#{student[:name]}, (#{student[:cohort].capitalize}) cohort."
-      print "\n#{student[:name]} likes #{student[:hobbies]}"
-      print "\n#{student[:name]} was born in #{student[:birthplace]}"
-      print "\n#{student[:name]} is #{student[:height]} tall"
-    else
-      print "No students start with letter A and are less than 12 characters long."
-    end
-
-    count -= 1
+def display
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def footer(students)
-  "\nOverall we have: #{students.length} great student(s).\n"
+def print_footer    
+  puts "Overall, we have #{students.length} great students"
 end
 
 def input_students
-  print "Please enter the names of the student(s).\n".center(60)
-  print "To finish, just hit return twice.\n".center(30)
-  # create an empty array 
-  students = []
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
   # get the first name
-  prompt = "> "
-  print prompt
   name = gets.chomp
-  while !name.empty? do 
-    # get cohort
-    print "What cohort does #{name} belong to?"
-    print "\n> "
-    cohort = gets.chomp.to_sym
-    cohort.length == 0 ? cohort = :December : cohort # check 
-    # get hobbies
-    print "What hobbies does #{name} have?"
-    print "\n> "  
-    hobbies = gets.delete("\n").delete("\r")
-    hobbies.length == 0 ? hobbies = :"I'm boring" : hobbies
-    # get country of birth
-    print "Where was #{name} born?"
-    print "\n> "
-    birthplace = gets.chomp
-    birthplace.length == 0 ? birthplace = :"Someplace" : birthplace
-    # get height
-    print " How tall is #{name}?"
-    print "\n> "
-    height = gets.chomp
-    birthplace.length == 0 ? height = :"Someheight" : height
-    
-    check_correct_inputs(name,cohort,hobbies,birthplace,height)
-
-    #add the student to the hash 
-    students << {:name => @name,
-                 :cohort => @cohort.to_sym,  
-                 :hobbies => @hobbies, 
-                 :birthplace => @birthplace,
-                 :height => @height
-    }
-
-    plural_check = students.length == 1 ? "student" : "students"
-
-    print "Now we have #{students.length} #{plural_check}."
-    # get another name from the user 
-    print "\nAdd another student? Alternatively, press enter to exit.\nName: "
+  # while the name is not empty, repeat this code
+  while !name.empty? do    
+    # add the student hash to the array
+    @students << {:name => name, :cohort => :november}    
+    puts "Now we have #{students.length} students"
+    # get another name from the user
     name = gets.chomp
-  end 
-  # return the array of students
-  students
-end
-
-def check_correct_inputs(name,cohort,hobbies,birthplace,height)
-
-  @name = name
-  @cohort = cohort 
-  @hobbies = hobbies
-  @birthplace = birthplace 
-  @height = height 
-  
-  puts "Name: #{name}, Cohort: #{cohort}, Hobbies: #{hobbies}, Birthplace: #{birthplace}, Height: #{height}."
-  
-  puts "Everything correct? Type 'exit' if so or type 'name', 'cohort', 'hobbies', 'birthplace' or 'height' to 'change'."
-  
-  check = gets.chomp 
-
-  until check == "exit"
-    case check.downcase
-      when "name" 
-        print "Name: "
-        @name = gets.chomp
-        print "Anything else you would like change? "
-        check = gets.chomp 
-      when "cohort"
-        print "Cohort: " 
-        @cohort = gets.chomp
-        print "Anything else you would like change? "
-        check = gets.chomp 
-      when "hobbies"
-        print "Hobbies: " 
-        @hobbies = gets.chomp
-        print "Anything else you would like change? "
-        check = gets.chomp
-      when "birthplace"
-        print "Birthplace: " 
-        @birthplace = gets.chomp
-        print "Anything else you would like change? "
-        check = gets.chomp
-      when "height"
-        print "Height: " 
-        @height = gets.chomp
-        print "Anything else you would like change? "
-        check = gets.chomp
-      else
-        print "Sorry, I don't understand: "
-        check = gets.chomp
-      end
   end
-  return @name, @cohort, @hobbies, @birthplace, @height
 end
 
+def interative_menu
+  # 1. print the menu and ask the user what to do 
+  # 2. read the input and save it into a variable
+  # 3. do what the user asked
+  # 4. repeat from step 1 
+end
 
-
-
-
- 
-
-print header
+print_header
 students = input_students
 display(students)
 print footer(students)
